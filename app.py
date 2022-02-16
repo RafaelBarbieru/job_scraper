e4b8dfa385682b0303f729e6d2325709085a9b01
RafaelCristianBarbieru <baracri00@gmail.com>
2022-02-16 15:16:53 +0100

    The program now detects the jobs list from the URL, but only the ones that appear on screen

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import time
 import os
+import json
 from os.path import join, dirname
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -15,11 +16,11 @@ def init_driver(headless):
     chrome_options = Options()
     if headless: chrome_options.add_argument("--headless")
     if headless: chrome_options.add_argument("--disable-gpu")
-    return webdriver.Chrome(options=chrome_options)    
+    return webdriver.Chrome(options=chrome_options)
 
 # Logs in the user on LinkedIn
 def linkedin_login_user():
-    print('===== Logging in user =====')
+    log('Logging in user')
     
     # Navigating to the signin page
     driver.get('https://www.linkedin.com/login?fromSignIn=true&amp;trk=guest_homepage-basic_nav-header-signin')
@@ -47,25 +48,49 @@ def get_linkedin_url(keywords, location):
             keywords_string += '%20OR%20'
     linkedin_url = f'https://www.linkedin.com/jobs/search/?currentJobId=2921025191&geoId=105646813&keywords={keywords_string}&location={location}'
     return linkedin_url
+
+def scrap_details():
+    log('Scraping details')
+    wait = WebDriverWait(driver, 5)
+    jobs = []
+    wait.until(ec.visibility_of_any_elements_located((By.CSS_SELECTOR, "li.jobs-search-results__list-item.occludable-update.p0.relative.ember-view"))) 
+    jobs_list = driver.find_elements(By.CSS_SELECTOR, "li.jobs-search-results__list-item.occludable-update.p0.relative.ember-view")
+    log(f"Jobs found: {len(jobs_list)}")
+    for job in jobs_list:
+        title = job.find_element(By.XPATH, ".//div/div/div/div[2]/div[1]/a").text
+        jobs.append({'title': title})
+        
+    return jobs
+
+
+def log(msg):
+    print(f'===== {msg} =====')
  
 
 """ ==================== MAIN EXECUTION ==================== """
 
-# Pre-configuration
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+try:
+    # Pre-configuration
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
 
-# Opening the site
-driver = init_driver(headless=False)
+    # Opening the site
+    driver = init_driver(headless=False)
 
-# Logging in the user
-linkedin_login_user()
+    # Logging in the user
+    linkedin_login_user()
+    log(f"User {os.environ.get('LINKEDIN_USERNAME')} logged in")
 
-# Navigating to LinkedIn job search with custom keywords and location
-keywords = ['backend', 'programador', 'java']
-location = 'Spain'
-browse_linkedin_jobs(keywords, location)
+    # Navigating to LinkedIn job search with custom keywords and location
+    keywords = ['backend', 'programador', 'java']
+    location = 'Spain'
+    browse_linkedin_jobs(keywords, location)
 
-# Closing the driver 
-time.sleep(10)
-driver.close()
+    # Scraping jobs details
+    jobs = scrap_details()
+    print(jobs)
+    
+finally:
+    print('===== Closing the driver =====')
+    # Closing the driver 
+    driver.close()
